736880a72d3ed053e472f5f45f881d6a286436f7
fayrose <laurenfay.rosenberg@mail.utoronto.ca>
2016-11-19 02:04:41 -0500

    Adds the tabs for sub-partials and replaces some of the UX design

--- WebServer/app/site/controllers/dash.ctrl.js
+++ WebServer/app/site/controllers/dash.ctrl.js
@@ -29,7 +29,27 @@
         //         htmlbodyHeightUpdate()
         //     });
         // });
-        this.welcome_message = "Hello!";
+        this.welcome_message = "Budget Buddy!";
+
+        // Function Bindings
+        this.change_tab = change_tab;
+
+        function change_tab(active) {
+          if (active == "tab1") {
+            $("#tab1").addClass("active")
+            $("#tab2").removeClass("active")
+            $("#tab3").removeClass("active")
+          }
+          else if (active == "tab2") {
+            $("#tab2").addClass("active")
+            $("#tab1").removeClass("active")
+            $("#tab3").removeClass("active")
+          }
+          else if (active == "tab3") {
+            $("#tab3").addClass("active")
+            $("#tab1").removeClass("active")
+            $("#tab2").removeClass("active")
+          }
+        }
     }
 })();
-
