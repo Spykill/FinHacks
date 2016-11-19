1c32b2fe4319dd20c0265977da85fa1b0af56357
Andriy Dzikh <andriydzikh@gmail.com>
2016-11-19 02:20:12 -0500

    Worked on AJAX API

--- WebServer/api/routes/user_routes.js
+++ WebServer/api/routes/user_routes.js
@@ -2,6 +2,52 @@ var express 	 = require('express');
 var router 		 = express.Router();
 
 
+function compute_category(transaction_list)
+{
+	return 0;
+}
+
+function calculate_average(people)
+{
+	var sums = {};
+	var denoms = {};
+	for(var i = 0; i < people.length; i++)
+	{
+		Object.keys(people[i].averages).forEach(function(key)
+		{
+			if(sums[key] == undefined)
+			{
+				sums[key] = 0;
+				denoms[key] = 0;
+			}
+			sums[key] += people[i].averages;
+			denoms[key] += 1;
+		});
+	}
+	Object.keys(sums).forEach(function(key)
+	{
+		if(denoms[key] != 0)
+		{
+			sums[key] /= denoms[key];
+		}
+	});
+	return sums;
+}
+
+function get_deltas(me, average)
+{
+	var deltas = {};
+	Object.keys(me.averages).forEach(function(key)
+	{
+		if(average[key] != undefined)
+		{
+			deltas[key] = me.averages[key] - average[key];
+		}
+	});
+
+	return deltas;
+}
+
 router.get('/login',function(req,res){
 	res.json({value:'You have reached a GET endpoint for login'});
 })
@@ -16,14 +62,19 @@ router.post('/login', function(req, res, next) {
 	coll.findOne({un: un}, {}, function(err, doc){
 		if(err)
 		{
-			res.send("Oh no! Something went wrong!");
+			res.status(500).send("Oh no! Something went wrong!");
 		}
 		else
 		{
-	    	res.redirect("success.html");
+	    	res.status(200).send("Good!");
 	    }
 	});
 });
+
+router.get('/signup',function(req,res){
+	res.json({value:'You have reached a GET endpoint for signup'});
+});
+
 router.post('/signup', function(req, res, next) {
 	var un = req.body.username;
 	var pw = req.body.password;
@@ -31,10 +82,108 @@ router.post('/signup', function(req, res, next) {
 	var db = req.db;
 
 	var coll = db.get('users');
-	coll.findOne({un: un}, {}, function(err, doc){
-		console.log(doc);
+	coll.insert({"un": un, "pw": pw, "transaction": [], "category": 0, "averages": {}}, function(err, doc){
+		if(err)
+		{
+			res.status(500).send("Oh no! Something went wrong!");
+		}
+		else
+		{
+	    	res.status(200).send("Good! " + un + " | " + pw);
+	    }
 	});
-    res.redirect("success.html");
+});
+
+router.post('/addtransaction', function(req, res, next) {
+	var un = req.body.username;
+
+	console.log(un);
+
+	var db = req.db;
+
+	// DO STUFF HERE
+	var transaction = {};
+	transaction.name = req.body.t_name;
+	transaction.amount = req.body.t_amount;
+	transaction.category = req.body.t_category;
+
+	var coll = db.get('users');
+	coll.findOne({"un": un}, function(err, doc){
+
+		if(err)
+		{
+			res.status(500).send("Oh no! Something went wrong! Couldn't find user");
+		}
+		else
+		{
+			doc.averages[transaction.category] *= doc.transaction.length;
+			doc.averages[transaction.category] += doc.transaction.amount;
+
+			doc.transaction.push(transaction);
+			category = compute_category(doc.transaction);
+			doc.averages[transaction.category] /= doc.transaction.length;
+
+			coll.updateOne({"un": un}, {$push: {"transaction": transaction}, $set: {"category": category, "averages": doc.averages}}, function(err, doc){
+				if(err)
+				{
+					res.status(500).send("Oh no! Something went wrong! Couldn't update the user");
+				}
+				else
+				{
+			    	res.status(200).send("Good!");
+			    }
+			});
+		}
+	});
+});
+
+router.post('/getdata', function(req, res, next) {
+	var un = req.body.username;
+
+	var db = req.db;
+
+	var coll = db.get('users');
+	coll.findOne({"un": un}, function(err, doc){
+		if(err)
+		{
+			res.status(500).send("Oh no! Something went wrong!");
+		}
+		else
+		{
+	    	res.status(200).send(doc);
+	    }
+	});
+});
+
+router.post('/compare', function(req, res, next){
+	var un = req.body.username;
+
+	var db = req.db;
+
+	var coll = db.get('users');
+	coll.findOne({"un": un}, function(err, doc){
+		if(err)
+		{
+			res.status(500).send("Oh no! Something went wrong!");
+		}
+		else
+		{
+			coll.find({"category": doc.category}, function(err, docs)
+			{
+				if(err)
+				{
+					res.status(500).send("Oh no! Something went wrong!");
+				}
+				else
+				{
+					docs = docs.toArray();
+					var deltas = get_deltas(doc, calculate_average(docs));
+
+					res.status(200).send(deltas);
+				}
+			});
+	    }
+	});
 });
 
 
--- WebServer/api/server.js
+++ WebServer/api/server.js
@@ -1,19 +1,23 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-var app = express();
+var path = require('path');
 
 var mongo = require('mongodb');
 var monk = require('monk');
 var db = monk('localhost:27017/finhacks');
 
-//app config
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static(__dirname + './../app/'));
-
 //route configuration
 var front_end = require('./routes/front_end');
 var user_routes = require('./routes/user_routes');
+
+var app = express();
+
+//app config
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+})); 
+app.use(express.static(__dirname + './../app/'));
 
 app.use(function(req,res,next){
     req.db = db;
